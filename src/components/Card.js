0908c93bba2ea7bd9f917217254f88bb180b9f43
Irina Turenko <turenko7irina@mail.ru>
2021-04-04 22:27:34 +0300

    css ups

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BsChevronDoubleDown } from 'react-icons/bs';
 import { BsChevronDoubleUp } from 'react-icons/bs';
-
+import { BsLink45Deg } from 'react-icons/bs';
 
 const Card = ({
     label,
@@ -12,12 +12,13 @@ const Card = ({
     const [showIngr, setShowIngr] = useState(false);
     return (
         <div className='card'>
-            <a href={url} className='label'>{label}</a>
+            <div className='label'>{label}</div>
             <img src={image} alt={label} />
             <br/>
             <div className='caret' onClick={() => { setShowIngr(!showIngr)}}> 
             {!showIngr ? (<><div></div> <BsChevronDoubleDown /> </>): <BsChevronDoubleUp />} </div>
             {showIngr && <ul className='ingr'>{ingredientLines}</ul>}
+            <a href={url} style={{fontSize:'2rem', color:'black'}}> <BsLink45Deg /></a>
         </div>
     )
 }
