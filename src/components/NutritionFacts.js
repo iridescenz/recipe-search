73af6ee9459434eb6d13541b1e2d9b051bbba6ee
Irina Turenko <turenko7irina@mail.ru>
2021-04-08 09:16:57 +0300

    upd nutrition

--- src/components/NutritionFacts.js
+++ src/components/NutritionFacts.js
@@ -4,25 +4,28 @@ import Footer from './Footer'
 import NutritionCard from './NutritionCard'
 import axios from 'axios'
 import { GoSearch } from 'react-icons/go'
+import dailyRecomendationValues from './vitaminValue'
 
 
 export const NutritionFacts = () => {
     const [product, setProduct] = useState('');
-    const [nutritionData, setNutritionData] = useState('')
+    const [productName, setProductName] = useState('');
+    const [nutritionData, setNutritionData] = useState('');
     const searchNutrition = (e) => {
         e.preventDefault();
+        setProductName(product);
         const apiKey = `ec4f9e290ce094b4adcf78bbfd65062e`;
         const apiId = `d3f05ade`;
         const res = axios.get(`https://api.edamam.com/api/nutrition-data?app_id=${apiId}&app_key=${apiKey}&ingr=${encodeURIComponent(product)}&img=yes`)
         .then(res => setNutritionData(res.data))
         .catch(er => console.log(er))
-        setProduct('')
+        setProduct('');
     }
 console.log(nutritionData)
     const nutrients = nutritionData && Object.entries(nutritionData.totalNutrients).map(el => Object.entries(el)).map((el, i) =>{
         const {label, quantity, unit} = el[1][1]; 
-        return <li key={i + Date.now()}> {`${label}: ${quantity.toFixed(2)} ${unit}`} </li>})
-    const { totalWeight } = nutritionData;
+        return <li key={i + Date.now()}> {`${label}: ${quantity.toFixed(0)} ${unit} ${9} % from daily recommended value`} </li>})
+    const { totalWeight, totalDaily } = nutritionData;
  
     return (
         <div className='main-component' >
@@ -40,8 +43,9 @@ console.log(nutritionData)
                     <button className='button' type='submit'>Search</button>
             </form>
             <div>
-                <NutritionCard nutrients={nutrients} 
-                                weight={  nutritionData && `Total weight: ${totalWeight.toFixed(0)} gram`} />
+                {nutritionData && <NutritionCard label={productName}
+                                nutrients={nutrients} 
+                                weight={`Total weight: ${totalWeight.toFixed(0)} gram`} />}
             </div>
            </div>
             <Footer />
---
+++ src/components/vitaminValue.js
@@ -0,0 +1,38 @@
+const dailyRecomendationValues = [
+    {name: 'Energy', value: 2000 },
+    {name: 'Fat', value: 50},
+    {name: 'Saturated', value: 20},
+    {name: 'Protein', value: 70},
+    {name: 'Trans', value: 2}, 
+    {name: 'Monounsaturated', value: 15},
+    {name: 'Polyunsaturated', value: 22},
+    {name: 'Carbs', value: 100},
+    {name: 'Fiber', value: 30},
+    {name: 'Sugars', value: 30},
+    {name: 'Protein', value: 70},
+    {name: 'Cholesterol', value: 300},
+    {name: 'Sodium', value: 3400},
+    {name: 'Calcium', value: 1000},
+    {name: 'Magnesium', value: 400},
+    {name: 'Potassium', value: 3000},
+    {name: 'Iron', value: 12}, 
+    {name: 'Zinc', value: 10},
+    {name: 'Phosphorus', value: 1000},
+    {name: 'Vitamin A', value: 700},
+    {name: 'Vitamin C', value: 80},
+    {name: 'Thiamin (B1)', value: 1}, 
+    {name: 'Riboflavin (B2)', value: 1.2},
+    {name: 'Niacin (B3)', value: 14},
+    {name: 'Vitamin B6', value: 1.5},
+    {name: 'Folate equivalent (total)', value: 'no data'},
+    {name: 'Folate (food)', value: 'no data'},
+    {name: 'Folic acid', value: 400},
+    {name: 'Vitamin B12', value: 2.4},
+    {name: 'Vitamin D', value: 10},
+    {name: 'Vitamin E', value: 10},
+    {name: 'Vitamin K', value: 120},
+    {name: 'Water', value: 1500}
+
+]
+
+export default dailyRecomendationValues
